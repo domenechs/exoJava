4e12e3a7d653981862a5992046922e6a43352109
DomenechS <sebastien.domenech@epita.fr>
2020-11-28 18:51:31 +0100

    Exercice String calculator

--- src/main/java/org/bnpp/exercicesjava/ExerciceTDD.java
+++ src/main/java/org/bnpp/exercicesjava/ExerciceTDD.java
@@ -2,25 +2,43 @@ package org.bnpp.exercicesjava;
 
 public class ExerciceTDD {
 
-    public static String transformerEntierEnString(int nombre) throws Exception {
-        if(nombre < 0){
-            throw (new Exception("No negative number allowed"));
+    public String transformerEntierEnString(int nombre) throws Exception {
+
+        if (nombre < 0) {
+            throw (new Exception("No negative numbers allowed"));
         }
-        if(nombre%3 == 0 && nombre%7 == 0){
-            return "FIZZQIX";
+        String stringReturn = String.valueOf(nombre);
+        stringReturn = stringReturn.replaceAll("0","*");
+        String stringTransf ="";
+        if (nombre % 3 == 0) {
+            stringTransf = "FIZZ";
+            stringReturn = stringTransf;
         }
-        if(nombre%3 == 0 && nombre%5 == 0){
-            return "FIZZBUZZ";
+        if (nombre % 5 == 0) {
+            stringTransf = stringTransf.concat("BUZZ");
+            stringReturn = stringTransf;
         }
-        if(nombre%3 == 0){
-            return "FIZZ";
-        }
-        if(nombre%5 == 0){
-            return "BUZZ";
-        }
-        if(nombre%7 == 0 && nombre != 7){
-            return "QIX";
-        }
-        return String.valueOf(nombre);
+        return stringReturn;
     }
+
+    public String add(String nombre) throws Exception {
+        if(nombre.isEmpty()){
+            return "0";
+        }
+        if(nombre.endsWith(",")){
+            throw (new Exception("Number expected but EOF found"));
+        }
+        int somme=0;
+        String[] chaine = nombre.split("[,\n]");
+
+        for (int i = 0; i < chaine.length; i++) {
+            if(chaine[i].isEmpty()){
+                throw (new Exception("Number expected but \\n found at position 6"));
+            }
+            somme = somme + Integer.parseInt(chaine[i]);
+        }
+
+        return String.valueOf(somme);
+    }
+
 }
--- src/test/java/org/bnpp/exercicejavatest/ExerciceTDDTest.java
+++ src/test/java/org/bnpp/exercicejavatest/ExerciceTDDTest.java
@@ -1,26 +1,53 @@
 package org.bnpp.exercicejavatest;
 
 import org.assertj.core.api.Assertions;
+import org.bnpp.exercicesjava.ExerciceTDD;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.catchThrowable;
-import static org.bnpp.exercicesjava.ExerciceTDD.transformerEntierEnString;
+
 
 public class ExerciceTDDTest {
+
+//    @BeforeAll
+//    public void beforeAll() {
+//        ExerciceTDD exo = new ExerciceTDD();
+//    }
 
     @Test
     @DisplayName("Test exercice TDD entier -> String")
     public void test1() throws Exception {
-        Assertions.assertThat(transformerEntierEnString(7)).isEqualTo("7");
-        Assertions.assertThat(transformerEntierEnString(3)).isEqualTo("FIZZ");
-        Assertions.assertThat(transformerEntierEnString(5)).isEqualTo("BUZZ");
-        Assertions.assertThat(transformerEntierEnString(15)).isEqualTo("FIZZBUZZ");
-        Throwable exception = catchThrowable(()->transformerEntierEnString(-5));
-        Assertions.assertThat(exception).isNotNull();
-        Assertions.assertThat(transformerEntierEnString(14)).isEqualTo("QIX");
-        Assertions.assertThat(transformerEntierEnString(21)).isEqualTo("FIZZQIX");
-        Assertions.assertThat(transformerEntierEnString(15)).isEqualTo("FIZZBUZZBUZZ");
-        Assertions.assertThat(transformerEntierEnString(33)).isEqualTo("FIZZFIZZFIZZ");
+        ExerciceTDD exo = new ExerciceTDD();
+        Assertions.assertThat(exo.transformerEntierEnString(1)).isEqualTo("1");
+        Assertions.assertThat(exo.transformerEntierEnString(25)).isEqualTo("BUZZ");
+        Assertions.assertThat(exo.transformerEntierEnString(2)).isEqualTo("2");
+        Assertions.assertThat(exo.transformerEntierEnString(3)).isEqualTo("FIZZ");
+        Assertions.assertThat(exo.transformerEntierEnString(6)).isEqualTo("FIZZ");
+        Assertions.assertThat(exo.transformerEntierEnString(5)).isEqualTo("BUZZ");
+        Assertions.assertThat(exo.transformerEntierEnString(15)).isEqualTo("FIZZBUZZ");
+        Assertions.assertThat(exo.transformerEntierEnString(33)).isEqualTo("FIZZ");
+        Assertions.assertThat(exo.transformerEntierEnString(101)).isEqualTo("1*1");
+        Throwable exception = catchThrowable(() -> exo.transformerEntierEnString(-1));
+        Assertions.assertThat(exception).hasMessage("No negative numbers allowed");
+
+    }
+
+    @Test
+    @DisplayName("Test exercice String calculator")
+    public void test2() throws Exception {
+        ExerciceTDD exo = new ExerciceTDD();
+        Assertions.assertThat(exo.add("1")).isEqualTo("1");
+        Assertions.assertThat(exo.add("1,2")).isEqualTo("3");
+        Assertions.assertThat(exo.add("")).isEqualTo("0");
+        Assertions.assertThat(exo.add("5,5,6,4")).isEqualTo("20");
+        Assertions.assertThat(exo.add("5,5\n6,4")).isEqualTo("20");
+        Throwable erreur = catchThrowable(() -> exo.add("175,2,\n354"));
+        Assertions.assertThat(erreur).hasMessage("Number expected but \\n found at position 6");
+        Throwable erreur2 = catchThrowable(() -> exo.add("1,3,"));
+        Assertions.assertThat(erreur2).hasMessage("Number expected but EOF found");
     }
 }
